791e81c0e5c9a93a415c4de9eef857873c41484c
Siyabonga-LNP-35 <ST10453541@rcconnect.edu.za>
2024-09-23 21:37:52 +0200

    Update Main class to handle user interaction for login and registration
    - Implemented input prompts for username and password.
    - Added logic to call the registration and login methods based on user input.

--- src/main/java/com/mycompany/login/Main.java
+++ src/main/java/com/mycompany/login/Main.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.login;
-
+//adding Scanner
+import java.util.Scanner;
 /**
  *
  * @author siyab
@@ -15,6 +16,39 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+            Scanner sc = new Scanner(System.in);
+            
+        // Registration of the user and Prompt user for information
+        System.out.println("Enter your first name:");
+        String firstName = sc.nextLine();
+        
+        System.out.println("Enter your last name:");
+        String lastName = sc.nextLine();
+        
+        System.out.println("Enter your username (must contain an underscore and be no more than 5 characters):");
+        String username = sc.nextLine();
+        
+        System.out.println("Enter your password (must be at least 8 characters, contain a capital letter, a number, and a special character):");
+        String password = sc.nextLine();
+        
+        Login user = new Login(firstName, lastName, username, password);
+        
+        String registrationMessage = user.registerUser();
+        System.out.println(registrationMessage);
+        
+        if (registrationMessage.equals("Username and password successfully captured.")) {
+            // know the user can Login
+            System.out.println("Enter your username to log in:");
+            String enteredUsername = sc.nextLine();
+            
+            System.out.println("Enter your password:");
+            String enteredPassword = sc.nextLine();
+            //user successfully loggedIn
+            boolean isLoggedIn = user.loginUser(enteredUsername, enteredPassword);
+            System.out.println(user.returnLoginStatus(isLoggedIn));
+              
+          }
+        
     }
-    
 }
