c47d8e9ca5376e403fbd7f635203de23ebe17100
Siyabonga-LNP-35 <ST10453541@rcconnect.edu.za>
2024-09-23 19:18:24 +0200

    Add basic registration and login functionality
    - Implemented methods for user registration, including input validation
      for usernames and passwords.
    - Added placeholders for login validation.

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -9,6 +9,20 @@ package com.mycompany.login;
  * @author siyab
  */
 public class Login {
+    //Declarations 
+    
+   private final String firstName;
+   private final String lastName;
+   private final String username;
+   private final String password;
+   
+   // Prompt user for information
+    public Login(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
